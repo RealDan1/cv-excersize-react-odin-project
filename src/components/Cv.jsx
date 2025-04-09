8084a5e569891931620ae34f5ed713cdd65291fe
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-09 15:52:24 +0200

    feat: change html for easier layout

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,16 +2,26 @@ export default function Cv({ cv }) {
   return (
     <div>
       <h1>{cv?.general?.name}'s CV</h1>
-      <p>Email: {cv?.general?.email}</p>
-      <p>Phone: {cv?.general?.phone}</p>
-      <p>School Name: {cv?.education?.schoolName}</p>
-      <p>Study Title: {cv?.education?.studyTitle}</p>
-      <p>Study Date: {cv?.education?.studyDate}</p>
-      <p>Company Name: {cv?.experience?.companyName}</p>
-      <p>Position Title: {cv?.experience?.positionTitle}</p>
-      <p>Responsibilities: {cv?.experience?.responsibilities}</p>
-      <p>Worked From: {cv?.experience?.dateFrom}</p>
-      <p>Worked Until: {cv?.experience?.dateUntil}</p>
+      <p>Email: </p>
+      <p>{cv?.general?.email}</p>
+      <p>Phone:</p>
+      <p>{cv?.general?.phone}</p>
+      <p>School Name: </p>
+      <p>{cv?.education?.schoolName}</p>
+      <p>Study Title: </p>
+      <p>{cv?.education?.studyTitle}</p>
+      <p>Study Date: </p>
+      <p>{cv?.education?.studyDate}</p>
+      <p>Company Name: </p>
+      <p>{cv?.experience?.companyName}</p>
+      <p>Position Title: </p>
+      <p>{cv?.experience?.positionTitle}</p>
+      <p>Responsibilities: </p>
+      <p>{cv?.experience?.responsibilities}</p>
+      <p>Worked From: </p>
+      <p>{cv?.experience?.dateFrom}</p>
+      <p>Worked Until: </p>
+      <p>{cv?.experience?.dateUntil}</p>
     </div>
   );
 }
